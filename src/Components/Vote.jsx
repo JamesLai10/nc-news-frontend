e66048280c253197578c410f7d2d3fcc437a4b8c
JamesLai10 <JamesLai2645@gmail.com>
2023-10-25 16:36:39 +0100

    added error message for offline voting

--- src/Components/Vote.jsx
+++ src/Components/Vote.jsx
@@ -3,17 +3,26 @@ import { updateArticleVotes } from "../utils/api";
 
 function Vote({ votes, article_id }) {
   const [userVotes, setUserVotes] = useState(0);
+  const [error, setError] = useState(null);
 
   const handleVote = (value) => {
     setUserVotes((currentVotes) => {
       return currentVotes + value;
     });
 
-    updateArticleVotes(article_id, value);
+    updateArticleVotes(article_id, value)
+      .then((response) => {
+        console.log(response.data.article.votes);
+      })
+      .catch((error) => {
+        setError("Vote could not be processed. Please try again.");
+        console.log("Error updating Votes: ", error);
+      });
   };
 
   return (
     <div>
+      {error && <p className="error-message">{error}</p>}
       <p>Votes: {votes + userVotes}</p>
       <button
         disabled={userVotes === 1}
