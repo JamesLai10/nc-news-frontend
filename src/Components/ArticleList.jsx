597053652b4df94d0997205cafbc700e22c4ca7b
JamesLai10 <JamesLai2645@gmail.com>
2023-10-27 11:15:28 +0100

    added sort

--- src/Components/ArticleList.jsx
+++ src/Components/ArticleList.jsx
@@ -6,20 +6,42 @@ function ArticleList() {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [sortOption, setSortOption] = useState("created_at desc");
 
   useEffect(() => {
     setLoading(true);
 
-    getArticles()
-      .then((response) => {
-        setArticles(response.data.articles);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setError(error);
-        setLoading(false);
-      });
-  }, []);
+    const fetchArticles = () => {
+      getArticles({ sort_by: sortOption })
+        .then((response) => {
+          setArticles(response.data.articles);
+          setLoading(false);
+        })
+        .catch((error) => {
+          setError(error);
+          setLoading(false);
+        });
+    };
+
+    fetchArticles();
+  }, [sortOption]);
+
+  const handleSortChange = (event) => {
+    const newSortOption = event.target.value;
+    setSortOption(newSortOption);
+  };
+
+  const sortArticles = (articles) => {
+    const [sortColumn, sortOrder] = sortOption.split(" ");
+
+    return articles.slice().sort((a, b) => {
+      if (sortOrder === "asc") {
+        return a[sortColumn] > b[sortColumn] ? 1 : -1;
+      } else {
+        return a[sortColumn] < b[sortColumn] ? 1 : -1;
+      }
+    });
+  };
 
   if (loading) {
     return <p className="loading">Loading...</p>;
@@ -29,11 +51,24 @@ function ArticleList() {
     return <p>Error: {error.message}</p>;
   }
 
+  const sortedArticles = sortArticles(articles);
+
   return (
     <div className="article-list">
       <h1>News Articles</h1>
+      <div>
+        <label htmlFor="sort">Sort by: </label>
+        <select id="sort" value={sortOption} onChange={handleSortChange}>
+          <option value="created_at desc">Newest</option>
+          <option value="created_at asc">Oldest</option>
+          <option value="comment_count desc">Most Comments</option>
+          <option value="comment_count asc">Least Comments</option>
+          <option value="votes desc">Most Votes</option>
+          <option value="votes asc">Least Votes</option>
+        </select>
+      </div>
       <ul>
-        {articles.map((article) => (
+        {sortedArticles.map((article) => (
           <li key={article.article_id}>
             <div>
               <h2 className="article-title">
